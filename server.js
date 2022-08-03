39a3558d68c63016c9b8024e1393c60cd0d601af
dcontrer83 <dcontrer83@yahoo.com>
2022-08-03 14:33:40 -0700

    Add api route

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const path = require('path');
+const api = require('./routes/index.js');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(express.static('public'));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+app.use('/api', api);
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'))
