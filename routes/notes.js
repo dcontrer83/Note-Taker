d14eae877908538b68bb64c34e06e2b8100a4d64
dcontrer83 <dcontrer83@yahoo.com>
2022-08-03 16:09:20 -0700

    Add DELETE request

--- routes/notes.js
+++ routes/notes.js
@@ -1,7 +1,9 @@
 const notes = require('express').Router();
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
+const db = require('../db/db.json');
 
+//GET request for notes
 notes.get('/', (req, res) => {
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if (err) {
@@ -14,6 +16,7 @@ notes.get('/', (req, res) => {
     })
 });
 
+//POST request for new notes
 notes.post('/', (req, res) => {
 const {title, text} = req.body;
 
@@ -23,9 +26,7 @@ const {title, text} = req.body;
             text,
             id: uuidv4(),
         };
-    
-        // readAndAppend(newNote, './db/db.json');
-        // res.json(`Note added successfully 🚀`);
+
         fs.readFile('./db/db.json', 'utf8', (err, data) => {
             if (err) {
                 console.error(err)
@@ -39,11 +40,26 @@ const {title, text} = req.body;
                 res.json(`note has been added`);
             }
         })
-
-
     } else {
         res.error('Error in adding note')
     }
 });
+
+//Delete request to delete single note
+notes.delete('/:id', (req, res) => {
+    if(req.params.id) {
+        const reviewId = req.params.id;
+        for (let i = 0; i < db.length; i++ ) {
+            const currentId = db[i];
+            if(currentId.id === reviewId) {
+                db.splice(i, 1);
+                fs.writeFile('./db/db.json', JSON.stringify(db), (err) => {
+                    err ? console.error(err) : console.log(`Note had been deleted`)
+                })
+                return res.send('Note had been deleted');
+            }
+        }
+    }
+})
 
 module.exports = notes;
